312f4038cb440777cd815f59f78603eed443d12f
Mithian Acuna <59997173+Butterpod@users.noreply.github.com>
2025-03-28 17:20:24 +0100

    Début des test avec AssertJ ( asserthat() ).
    Probleme avec la methode "containsExactlyInAnyOrder()" pour un test, il ne reconnait pas la methode.
    
    Peut etre un probleme de dépendance maven, ou IntelliJ.
    
    Version maven AssertJ-core : 4.0.0-M1

--- src/main/java/org/example/testing/Calculator.java
+++ src/main/java/org/example/testing/Calculator.java
@@ -1,31 +1,44 @@
 package org.example.testing;
 import java.lang.Math.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Calculator {
 
-    public int add(int a, int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public double div(double a, double b){
-        return (double) Math.round(a / b * 100) /100;
+    public double div(double a, double b) {
+        return (double) Math.round(a / b * 100) / 100;
     }
 
-    public int mul(int a,int b){
-        return a*b;
+    public int mul(int a, int b) {
+        return a * b;
     }
 
-    public int diff(int a,int b){
-        return a-b;
+    public int diff(int a, int b) {
+        return a - b;
     }
 
     public void longCalculation() {
         try {
             // Attendre 2 secondes
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    public Set<Integer> digitsSet(int number) {
+        Set<Integer> integers = new HashSet<Integer>();
+        String numberString = String.valueOf(number);
+
+        for (int i = 0; i < numberString.length(); i++) {
+            if (numberString.charAt(i) != '-') {
+                integers.add(Integer.parseInt(numberString, i, i + 1, 10));
+            }
+        }
+        return integers;
+    }
 }
--- src/test/java/org/example/testing/CalculatorTest.java
+++ src/test/java/org/example/testing/CalculatorTest.java
@@ -1,14 +1,16 @@
 package org.example.testing;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import org.assertj.core.api.AbstractByteArrayAssert;
 import java.text.MessageFormat;
 import java.time.Duration;
 import java.time.Instant;
-
+import java.util.Set;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
@@ -48,14 +50,14 @@ public class CalculatorTest {
 
 
 
-
     @Test
     void testAddTwoNumbers(){
         int a =2;
         int b =3;
 
         int somme=calculator.add(a,b);
-        assertEquals(5,somme);
+        assertThat(somme).isEqualTo(5);
+
     }
 
     @Test
@@ -65,7 +67,8 @@ public class CalculatorTest {
 
         int mul=calculator.mul(a,b);
 
-        assertEquals(72,mul);
+        assertThat(mul).isEqualTo(72);
+        //assertEquals(72,mul);
     }
 
     @ParameterizedTest(name = "{0} x 0 doit être égal à 0")
@@ -73,8 +76,8 @@ public class CalculatorTest {
     public void multiplication_par_zero(int arg) {
 
         int actualResult = calculator.mul(arg, 0);
-
-        assertEquals(0, actualResult);
+        assertThat(actualResult).isEqualTo(0);
+        //assertEquals(0, actualResult);
 
     }
 
@@ -83,7 +86,8 @@ public class CalculatorTest {
     public void add_shouldReturnTheSum_ofMultipleIntegers(int arg1, int arg2, int expectResult) {
 
         int actualResult = calculator.add(arg1, arg2);
-        assertEquals(expectResult, actualResult);
+        assertThat(actualResult).isEqualTo(expectResult);
+        //assertEquals(expectResult, actualResult);
 
     }
 
@@ -92,6 +96,17 @@ public class CalculatorTest {
     public void longCalculationTest() {
 
         calculator.longCalculation();
+    }
+
+    @Test
+    public void listDigits_shouldReturnsTheListOfDigits_ofPositiveInteger() {
+
+        // GIVEN
+        int number = 95897;
+        // WHEN
+        Set<Integer> actualDigits = calculator.digitsSet(number);
+        // THEN
+        assertThat(actualDigits).containsExactlyInAnyOrder(5, 7, 8, 9);
     }
 
 }
